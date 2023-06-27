5df8634bad9bf141bb2003a43b19e9a2ba43fa36
EshuShango <tobiakinyemi21@gmail.com>
2023-06-27 18:11:19 -0500

    api Gateway implementation

--- got_app_v2/server/server/src/index.ts
+++ got_app_v2/server/server/src/index.ts
@@ -8,20 +8,32 @@ import cors from "cors";
 import proxy from "express-http-proxy";
 
 dotenv.config();
+export const APIgate = process.env.SERVER_PORT || 8000;
 export const startCardServer = async () => {
   try {
-    const APIgate = 8000;
-    // const APIgate = process.env.SERVER_PORT || 8000;
+    // const APIgate = 8000;
+    const APIgate = process.env.SERVER_PORT ;
+    
+    const userSrvr = process.env.SERVER_PORT ;
+    const authSrvr= process.env.AUTH_URL ;
+    const cardSrvr =  process.env.USER_URL;
+    const promptSrvr= process.env.PROMPT_URL;
 
     const app = express();
 
     // Database connection
 
     // await createApp(app);
-    app.use("/users", proxy("http://localhost:8001"));
-    app.use("/auth", proxy("http://localhost:8002"));
-    app.use("/cards", proxy("http://localhost:8003")); //cards
-    app.use("/prompts", proxy("http://localhost:8005"));
+    // app.use("/users", proxy("http://localhost:8001"));
+    // app.use("/auth", proxy("http://localhost:8002"));
+    // app.use("/cards", proxy("http://localhost:8003")); //cards
+    // app.use("/prompts", proxy("http://localhost:8005"));
+    // // app.use("/decks", proxy("http://localhost:8004"));
+    //!
+    app.use("/users", proxy("userSrvr"));
+    app.use("/auth", proxy("authSrvr"));
+    app.use("/cards", proxy("cardSrvr")); //cards
+    app.use("/prompts", proxy("promptSrvr"));
     // app.use("/decks", proxy("http://localhost:8004"));
 
     app.use("/", (req: Request, res: Response, next: NextFunction) => {
@@ -32,9 +44,11 @@ export const startCardServer = async () => {
       console.log(`Server Listening on port: http://localhost:${APIgate}`);
     });
 
-    // app.listen(proxy(), () => {
-    //   console.log(`Server Listening on port: ${config.serverPort}`);
-    // });
+//     app.listen(proxy(APIgate, {
+//   proxyErrorHandler: function(err, res, next) {
+//     next(err);
+//   }
+// }));
     // app.listen(APIgate, () => {
     //   console.log(`Server Listening on port: ${APIgate}`);
     // });
@@ -50,13 +64,15 @@ export const startCardServer = async () => {
 startCardServer();
 
 //! implement api gateway
+// dotenv.config();
 // export const startCardServer = async () => {
 //   try {
+//     // / export const APIgate = process.env.SERVER_PORT || 8000;
 //     const app = express();
 
 //     // Database connection
 
-//     await createApp(app);
+//     // await createApp(app);
 //     app.use("/users", proxy(config.userUrl));
 //     app.use("/auth", proxy(config.authUrl));
 //     app.use("/cards", proxy(config.cardUrl));
